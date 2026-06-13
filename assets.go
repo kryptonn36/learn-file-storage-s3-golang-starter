6d10bc0e2a93c63207691e3f34a64009475964da
kryptonn36 <srai325356@gmail.com>
2026-06-13 22:00:00 +0530

    cache updated

--- assets.go
+++ assets.go
@@ -1,12 +1,47 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
+
+	// "github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
 	if _, err := os.Stat(cfg.assetsRoot); os.IsNotExist(err) {
-		return os.Mkdir(cfg.assetsRoot, 0755)
+		return os.Mkdir(cfg.assetsRoot, 0o755)
 	}
 	return nil
+}
+
+func getAssetPath(mediaType string) string {
+	ext := mediaTypeToExt(mediaType)
+	path := randomBase64string()
+	return fmt.Sprintf("%s%s", path, ext)
+}
+
+func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
+	return filepath.Join(cfg.assetsRoot, assetPath)
+}
+
+func (cfg apiConfig) getAssetURL(assetPath string) string {
+	return fmt.Sprintf("http://localhost:%s/assets/%s", cfg.port, assetPath)
+}
+
+func mediaTypeToExt(mediaType string) string {
+	parts := strings.Split(mediaType, "/")
+	if len(parts) != 2 {
+		return ".bin"
+	}
+	return "." + parts[1]
+}
+
+func randomBase64string() string {
+	key := make([]byte, 32)
+	rand.Read(key)
+	return base64.RawURLEncoding.EncodeToString(key)
 }
